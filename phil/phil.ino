811e2a29dc1830a8ab7990524349885a08275763
psabaty <psabaty@pagesjaunes.fr>
2023-03-04 10:08:01 +0100

    Ajout 2eme sonde ampere sur sortie

--- phil/phil.ino
+++ phil/phil.ino
@@ -8,14 +8,15 @@
 
 const bool SIMULATION_FOR_DEBUG = false;
 
-unsigned long REQUEST_DELAY_MS = 3000;
+unsigned long REQUEST_DELAY_MS = 2100;
 const double THEORICAL_MAX_LOAD_W = 2000;
-const double MAX_LOAD_REQUEST_W = 750;
-const double MIN_LOAD_REQUEST_FOR_START = 150;
-const double MIN_LOAD_REQUEST_FOR_STOP = 100;
-const double POWER_INJECTION_TARGET_W = 15;
+const double MAX_LOAD_REQUEST_W = 1000;
+const double MIN_LOAD_REQUEST_FOR_START = 200;
+const double MIN_LOAD_REQUEST_FOR_STOP = 150;
+const double POWER_INJECTION_TARGET_W = 25;
 
-int sct013_pin = 1;
+int injection_sct013_pin = 1;
+int consumption_sct013_pin = 0;
 int basseTensionAC_pin = 2;
 int ledOptocoupleur_pin = 9; 
 int button_pin = 3; 
@@ -28,10 +29,12 @@ int lastLedRequest = 0;
 double baseLedRequestForSimulation = 0;
 
 double injectedEnergy_Wh = 0.0; 
-double consumedEnergy_Wh = 0.0;
+double consumedEnergy_Wh = 0.0; 
+double overConsumedEnergy_Wh = 0.0;
 double lastLoopInjectedEnergy_Ws = 0;
 double lastLoopConsumedEnergy_Ws = 0;
 int injectedPower_W = 0;
+int consumedPower_W = 0;
 int debug_injectedPower = -1;
 int simulatedPower_W = 0;
 
@@ -43,6 +46,7 @@ int screenNumber = 1;
 
 #ifndef TINKERCAD 
 EnergyMonitor emon1;
+EnergyMonitor emon2;
 #endif
 LiquidCrystal_I2C lcd(0x27,20,4); 
 
@@ -53,12 +57,14 @@ void setup()
 
   Serial.begin(9600);
   
-  int sct013_calibration = 46;
+#ifndef TINKERCAD 
   double basseTensionAC_calibration = 200;
     
-#ifndef TINKERCAD 
-  emon1.current(sct013_pin, sct013_calibration);    
+  emon1.current(injection_sct013_pin, 46);    
   emon1.voltage(basseTensionAC_pin, basseTensionAC_calibration, 1.7);  
+ 
+  emon2.current(consumption_sct013_pin, 20);    
+  emon2.voltage(basseTensionAC_pin, basseTensionAC_calibration, 1.7);  
 #endif
   lcd.init();                      
   lcd.backlight();
@@ -86,10 +92,14 @@ void loop_directTarget()
 #ifndef TINKERCAD 
   emon1.calcVI(20,2000); 
   injectedPower_W = -1 * int(emon1.realPower);
+
+  emon2.calcVI(20,2000); 
+  consumedPower_W = int(emon2.realPower);
 #endif
   // faire durer la boucle au moins 0.5 seconde
   delay(max(500 - (millis() - lastLoopTime_ms), 0));
   long lastLoopDuration_ms = (millis() - lastLoopTime_ms);
+  bool isLoadActive = (lastpowerRequest_W > 0);
   
 
   // Pour debug, forcer la valeur de injectedPower_W depuis l'interface série
@@ -108,7 +118,7 @@ void loop_directTarget()
   // Seulement si la dernière consigne a été appliquée depuis plus de 3 secondes
   hasRecentRequest = ( millis()-lastAppliedRequestTime_ms < REQUEST_DELAY_MS );
   if( ! hasRecentRequest ) {
-
+    // V1 : 
     // si injectedPower_W est positif
     //   alors on augmente la consigne avec cette puissance (en valeur absolue)
     //   en l'occurence on soustrait cette valeur negative -> augmentation
@@ -116,15 +126,26 @@ void loop_directTarget()
     //   alors on baisse la consigne avec cette puissance 
     //   en l'occurence on soustrait cette valeur positive -> baisse
     // en résumé, on applique toujours la même formule :
+    // powerRequest_W = powerRequest_W + injectedPower_W
+    
+    // V2 :
+    // La cible n'est plus Pi = 0, mais Pi ~15W, pour eviter la sur-consommation pendant le tracking
+    // si le delta est negatif, on l'applique totalement
+    // si le delta est positif, on l'appplique partiellement, pour limiter les oscillations autour de la cible
+    // ----> DESACTIVE : empeche le demarrage avant Pi > 2*MIN_LOAD_REQUEST_FOR_START
+    // double delta_W = injectedPower_W - POWER_INJECTION_TARGET_W;
+    // powerRequest_W = powerRequest_W + ((delta_W<0)?delta_W:(delta_W*0.5));
+    
+    // V3 :
+    // La cible n'est plus Pi = 0, mais Pi ~15W, pour eviter la sur-consommation pendant le tracking
     double delta_W = injectedPower_W - POWER_INJECTION_TARGET_W;
-    powerRequest_W = powerRequest_W + (delta_W / 2);
+    powerRequest_W = powerRequest_W + delta_W;
     
     // si la consigne est superieure au max de la charge, on plafonne
     powerRequest_W = min(powerRequest_W, MAX_LOAD_REQUEST_W);
     
     // si la consigne est inferieure à la puissance de declechement ET que la charge n'est pas allumée, on met à zero 
     // si la consigne est inferieure à la puissance de coupure on met à zero  
-    bool isLoadActive = (lastpowerRequest_W > 0);
     if( powerRequest_W < MIN_LOAD_REQUEST_FOR_START && !isLoadActive){ powerRequest_W = 0; }
     if( powerRequest_W < MIN_LOAD_REQUEST_FOR_STOP){ powerRequest_W = 0; }
 
@@ -142,11 +163,15 @@ void loop_directTarget()
   }
 
   // calculs cumuls energie 
-  if(injectedPower_W>0.0) {
-    lastLoopInjectedEnergy_Ws = injectedPower_W * ((double)lastLoopDuration_ms/1000);
+  lastLoopInjectedEnergy_Ws = injectedPower_W * ((double)lastLoopDuration_ms/1000);
+  if(lastLoopInjectedEnergy_Ws>0.0) {
     injectedEnergy_Wh = injectedEnergy_Wh + (lastLoopInjectedEnergy_Ws/3600);
   }
-  lastLoopConsumedEnergy_Ws = powerRequest_W * ((double)lastLoopDuration_ms/1000);
+  if(lastLoopInjectedEnergy_Ws<0.0 && isLoadActive) {
+    overConsumedEnergy_Wh = overConsumedEnergy_Wh - (lastLoopInjectedEnergy_Ws/3600);
+  }
+  // lastLoopConsumedEnergy_Ws = powerRequest_W * ((double)lastLoopDuration_ms/1000);
+  lastLoopConsumedEnergy_Ws = consumedPower_W * ((double)lastLoopDuration_ms/1000);
   consumedEnergy_Wh = consumedEnergy_Wh + (lastLoopConsumedEnergy_Ws/(3600));
 
   // Affichages LCD
@@ -199,21 +224,14 @@ void displaytoLCD() {
   int voltRequest_V = int(powerToVolts(powerRequest_W));
 
   if(screenChangeRequest==0) { screenNumber++; }
-  if(screenNumber>2) { screenNumber=1; }
-
-  //lcd.clear();
-  //char message0[16];   sprintf(message0, "%d/ Pi:%4dW", screenNumber, injectedPower_W);
-  //lcd.setCursor(0,0);  lcd.print(String(message0));
-  //lcd.setCursor(0,0);  lcd.print(String(screenNumber) + "-"+ String(hasRecentRequest ? "-" : " ")+" Pi=" + injectedPower_W + "W ");
-    
+  if(screenNumber>3) { screenNumber=1; }
 
   switch (screenNumber)
   {
-  case 1:lcd.setCursor(0,1);  
+  case 1:  
     lcd.setCursor(0,0);  lcd.print("Pi:" + String(injectedPower_W) + "W "+String(hasRecentRequest ? "*" : " ")+"    ");
-    //char message0[16];   snprintf(message0, 16, "Pi:%5dW %s", int(powerRequest_W), (hasRecentRequest ? "*" : " "));
-    //lcd.setCursor(0,0);  lcd.print(String(message0));
-    char message1[16];   snprintf(message1, 16, "R:%3dW %3dV %d ", int(powerRequest_W), int(voltRequest_V), ledRequest);
+    // char message1[16];   snprintf(message1, 16, "R:%3dW %3dV %d ", int(powerRequest_W), int(voltRequest_V), ledRequest);
+    char message1[16];   snprintf(message1, 16, "R=%3dW - k=%d ", int(consumedPower_W), ledRequest);
     lcd.setCursor(0,1);  lcd.print(String(message1));
     
     break;
@@ -223,6 +241,10 @@ void displaytoLCD() {
     lcd.setCursor(0,1);  lcd.print(String("Ea=" + String(consumedEnergy_Wh / 1000, 3) + "KWh    "));
     break;
     
+  case 3:
+    lcd.setCursor(0,0);  lcd.print(String("Esurconso = "));
+    lcd.setCursor(0,1);  lcd.print(String(overConsumedEnergy_Wh / 1000, 3) + "KWh    ");
+    break;
   default:
     lcd.setCursor(0,1);  lcd.print(String("*****"));
     break;
@@ -233,6 +255,7 @@ void displaytoLCD() {
 }
 
 void displayToSerial() {
+  /*
   Serial.print("Pi:");   Serial.print(injectedPower_W);
   //Serial.print(",Ei:"); Serial.print(injectedEnergy_Wh);
   Serial.print(",Preq:"); Serial.print(powerRequest_W);
@@ -240,8 +263,15 @@ void displayToSerial() {
   //Serial.print(",Psim:");  Serial.print(simulatedPower_W);
   Serial.print(",hRR:");  Serial.print(hasRecentRequest);
   //Serial.print(",e:");  Serial.print(lastLoopInjectedEnergy_Ws);
-  Serial.print(",t:");  Serial.print((millis() - lastLoopTime_ms));
-  Serial.print(",c:");  Serial.print(loopCount);
+  */
+  Serial.print(emon2.realPower); Serial.print(",");
+ Serial.print(emon2.apparentPower); Serial.print(",");
+ Serial.print(emon2.Vrms); Serial.print(",");
+ Serial.print(emon2.Irms); Serial.print(",");
+ Serial.print(emon2.powerFactor); Serial.println(","); 
+
+  //Serial.print(",t:");  Serial.print((millis() - lastLoopTime_ms));
+  //Serial.print(",c:");  Serial.print(loopCount);
   Serial.println();
 
 }
